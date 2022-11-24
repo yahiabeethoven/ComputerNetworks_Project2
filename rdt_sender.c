054e43aa7f2226cbc8d87abd51f85de322037370
yahiabeethoven <ybt214@nyu.edu>
2022-11-24 19:37:43 +0400

    fixed some stuff

--- rdt_sender.c
+++ rdt_sender.c
@@ -127,7 +127,7 @@ void *send_packet (void *arguments)
 
     init_timer(RETRY, resend_packets);                                                  // initialize the timer
 
-    int var = 0;
+    // int var = 0;
 
     while (1) 
     {
@@ -172,57 +172,64 @@ void *send_packet (void *arguments)
                 sndpkt = make_packet(0);
                 sendto(sockfd, sndpkt, TCP_HDR_SIZE,  0, (const struct sockaddr *)&serveraddr, serverlen);
                 VLOG(INFO, "End-of-file has been reached");
-                FILE *senderTest = fopen("../ComputerNetworks_Project2/highwayGold","r");
+                // cellularGold
+                FILE *senderTest = fopen("../ComputerNetworks_Project2/cellularGold","r");
+                // rapidGold
+                // FILE *senderTest = fopen("../ComputerNetworks_Project2/rapidGold","r");
+                // highwayGold
+                // FILE *senderTest = fopen("../ComputerNetworks_Project2/highwayGold","r");
                 if (senderTest){
                     printf("opened sender\n");
                 }
                 // tests to see the error in mahimahi
                 //----------------------------------------------------------------
-                // FILE *receiverTest = fopen("../com.c","r");
-                // if (receiverTest) {
-                //     printf("opened receiver\n");
-                // }
-                // fseek(senderTest,0,SEEK_END);
-                // long int sender_size=ftell(senderTest);
-                // printf("receiver size proper\n");
-                // fseek(senderTest,0,SEEK_SET);
-                // printf("fseek 1 proper\n");
-                // char* sender_contents; 
-                // sender_contents=(char*)malloc(sender_size);
-                // printf("sender contents proper\n");
-                // fread(sender_contents,sender_size,1,senderTest);
+                FILE *receiverTest = fopen("../com.c","r");
+                if (receiverTest) {
+                    printf("opened receiver\n");
+                }
+                fseek(senderTest,0,SEEK_END);
+                long int sender_size=ftell(senderTest);
+                printf("receiver size proper\n");
+                fseek(senderTest,0,SEEK_SET);
+                printf("fseek 1 proper\n");
+                char* sender_contents; 
+                sender_contents=(char*)malloc(sender_size);
+                
+                fread(sender_contents,sender_size,1,senderTest);
 
-                // fseek(receiverTest,0,SEEK_END);
-                // long int receiver_size=ftell(receiverTest);
-                // printf("receiver size proper\n");
-                // fseek(receiverTest,0,SEEK_SET);
-                // printf("fseek 2 proper\n");
-                // char* receiver_contents; 
-                // receiver_contents=(char*)malloc(receiver_size);
-                // fread(receiver_contents,receiver_size,1,receiverTest);
+                fseek(receiverTest,0,SEEK_END);
+                long int receiver_size=ftell(receiverTest);
+                
+                fseek(receiverTest,0,SEEK_SET);
+                printf("fseek 2 proper\n");
+                char* receiver_contents; 
+                receiver_contents=(char*)malloc(receiver_size);
+                fread(receiver_contents,receiver_size,1,receiverTest);
 
-                // int matchCounter = 0; 
-                // int wrongCounter = 0;
-                // for (int i = 0; i < receiver_size; i++) {
-                //         if (sender_contents[i] == receiver_contents[i]) {
-                //             matchCounter++;
-                //             printf("match: %d\n",matchCounter);
-                //         }
-                //         else {
-                //             wrongCounter++;
-                //             printf("wrong: %d\n",wrongCounter);
-                //         }
-                //     } 
-                // if (sender_size == receiver_size) {
-                     
-                // }
-                // else {
-                //     printf("sizes mismatch\n");
-                // }
-                // free(sender_contents);
-                // printf("free sender memory\n");
-                // free(receiver_contents);
-                // printf("free receiver memory\n");
+                int matchCounter = 0; 
+                int wrongCounter = 0;
+                
+                if (sender_size == receiver_size) {
+                    for (int i = 0; i < sender_size; i++) {
+                        if (sender_contents[i] == receiver_contents[i]) {
+                            matchCounter++;
+                            printf("match: %d\n",matchCounter);
+                        }
+                        else {
+                            wrongCounter++;
+                            printf("wrong: %d\n",wrongCounter);
+                        }
+                    }      
+                }
+                else {
+                    printf("sizes mismatch\n");
+                }
+                printf("sender contents proper, %ld\n",sender_size);
+                printf("receiver size proper, %ld\n",receiver_size);
+                free(sender_contents);
+                printf("free sender memory\n");
+                free(receiver_contents);
+                printf("free receiver memory\n");
                 //---------------------------------------------------------------- 
                 end_loop = 1; 
                                                                      // let the program end when it reaches EOF
